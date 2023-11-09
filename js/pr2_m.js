5b43bc9bfd4eb581470f1c3fad61a965ad307b7b
jjs06 <ohyoojin12@gmail.com>
2023-11-09 11:43:40 +0900

    2023.11.09

--- js/pr2_m.js
+++ js/pr2_m.js
@@ -4,117 +4,170 @@ window.onload = function () {
 
     dn.onclick = function () {
         console.log(dn.querySelector('img').getAttribute('src'));
-        if(dn.querySelector('img').getAttribute('src') == 'img/night.png'){
-            
+        if (dn.querySelector('img').getAttribute('src') == 'img/night.png') {
+
+            // change button (.dn)
             this.querySelector('img').src = 'img/day.png';
             let mn = document.getElementsByClassName('line');
-            for (var i = 0; i < mn.length; i++) {
+            for (let i = 0; i < mn.length; i++) {
                 mn[i].style.backgroundColor = '#000';
             }
-    
+
+            // button all
             let btn = document.getElementsByTagName('button');
-            for (var j = 0; j < btn.length; j++) {
+            for (let j = 0; j < btn.length; j++) {
                 btn[j].style.color = '#000';
                 btn[j].style.border = '2px solid #000';
             }
-    
+
+            
             document.getElementsByClassName('logo1')[0].querySelector('img').src = 'img/logo2.png';
-    
+
             document.body.style.backgroundColor = '#fff';
             wrap.style.color = '#000';
-    
+
             document.querySelector('html').style.setProperty("--ac", "#000");
             document.querySelector('html').style.setProperty("--bc", "#fff");
-    
+
             document.getElementsByClassName('sc7')[0].style.backgroundColor = '#fffadb';
-    
+
+            
             document.getElementById('slider').querySelector('video').src = 'img/video4.mp4';
-    
+
+            // sc1
             document.getElementsByClassName('m1')[0].querySelector('img').src = 'img/img1m.jpg';
             document.getElementsByClassName('m1')[1].querySelector('img').src = 'img/img2m.jpg';
             document.getElementsByClassName('m1')[2].querySelector('img').src = 'img/img3m.jpg';
-    
+
+            // sc2
             document.getElementsByClassName('m2')[0].querySelector('img').src = 'img/test31.jpg';
             document.getElementsByClassName('m3')[0].querySelector('img').src = 'img/img5m.jpg';
-    
+
+            
             document.getElementsByClassName('sc3')[0].style.backgroundImage = "url('img/test27.jpg')";
-    
+
+            // sc5
             document.getElementsByClassName('sc5')[0].querySelector('img').src = 'img/test26.jpg';
-    
-            document.getElementsByClassName('m6')[0].querySelector('img').src = 'img/test28.jpg';
-            document.getElementsByClassName('b3')[0].querySelector('h3').innerText = 'The Breakfast Clup'
-            document.getElementsByClassName('b3')[0].querySelector('p').innerText = '11 Southwark St, London SE1 1RQ'
-    
-            document.getElementsByClassName('m6')[1].querySelector('img').src = 'img/test29.jpg';
-            document.getElementsByClassName('b3')[1].querySelector('h3').innerText = 'Ozone Coffee Roasters'
-            document.getElementsByClassName('b3')[1].querySelector('p').innerText = '11 Leonard St, London EC2A 4AQ'
-    
-            document.getElementsByClassName('m6')[2].querySelector('img').src = 'img/test30.jpg';
-            document.getElementsByClassName('b3')[2].querySelector('h3').innerText = 'Half Cup'
-            document.getElementsByClassName('b3')[2].querySelector('p').innerText = '100-102 Judd St, London WC1H 9NT'
-    
-    
-    
+
+            //sc6
+
+            let img = ['img/test33-1.jpg', 'img/test34.jpg', 'img/test35.jpg'];
+            let place = [];
+
+            for(let i= 0; i > img.length; i++){
+
+            }
+            document.getElementsByClassName('m5')[0].querySelector('img').src = 'img/test33-1.jpg';
+
+            document.getElementsByClassName('tbox7')[0].querySelector('h3').innerText = 'The Apple Market';
+
+            document.getElementsByClassName('tbox7')[0].querySelector('p').innerText = ' Apple Market Covent Garden';
+
+            document.getElementsByClassName('tbox7')[0].querySelector('table tr:first-child>td').innerText = '21, The Market Building, London WC2E 8RD';
+
+            document.getElementsByClassName('tbox7')[0].querySelector('table tr:last-child>td').innerText = '-';
+
+            document.getElementsByClassName('m5')[1].querySelector('img').src = 'img/test34.jpg';
+
+            document.getElementsByClassName('tbox7')[1].querySelector('h3').innerText = 'Trafalgar Square';
+
+            document.getElementsByClassName('tbox7')[1].querySelector('p').innerText = ' Trafalgar Square';
+
+            document.getElementsByClassName('tbox7')[1].querySelector('table tr:first-child>td').innerText = 'Trafalgar Sq, London WC2N 5DS';
+
+            document.getElementsByClassName('tbox7')[1].querySelector('table tr:last-child>td').innerText = '-';
+
+            document.getElementsByClassName('m5')[2].querySelector('img').src = 'img/test35.jpg';
+
+            document.getElementsByClassName('tbox7')[2].querySelector('h3').innerText = 'Big Ben';
+
+            document.getElementsByClassName('tbox7')[2].querySelector('p').innerText = 'Big Ben';
+
+            document.getElementsByClassName('tbox7')[2].querySelector('table tr:first-child>td').innerText = 'SW1A 0AA London';
+
+            document.getElementsByClassName('tbox7')[2].querySelector('table tr:last-child>td').innerText = '-';
+
+            //sc7
+            let img2 = ['img/test28.jpg', 'img/test29.jpg', 'img/test30.jpg'];
+            let r_name = ['The Breakfast Clup', 'Ozone Coffee Roasters', 'Half Cup'];
+            let adress = ['11 Southwark St, London SE1 1RQ', '11 Leonard St, London EC2A 4AQ', '100-102 Judd St, London WC1H 9NT'];
+
+            for (let i = 0; i <img2.length; i++) {
+                document.getElementsByClassName('m6')[i].querySelector('img').src = img2[i];
+                document.getElementsByClassName('b3')[i].querySelector('h3').innerText = r_name[i];
+                document.getElementsByClassName('b3')[i].querySelector('p').innerText = adress[i];
+            }
+
+            //sc8
             document.getElementsByClassName('m7')[0].querySelector('img').src = 'img/test24-1.jpg';
             document.getElementsByClassName('m8')[0].querySelector('img').src = 'img/test25-1.jpg';
-    
+
+            //footer
             document.getElementsByClassName('logo2')[0].querySelector('img').src = 'img/logo2.png';
         }
 
-        else{
+        else {
+
+            // change button (.dn)
             this.querySelector('img').src = 'img/night.png';
             let mn = document.getElementsByClassName('line');
-            for (var i = 0; i < mn.length; i++) {
-                mn[i].style.backgroundColor = '#000';
+            for (let i = 0; i < mn.length; i++) {
+                mn[i].style.backgroundColor = '#fff';
             }
-    
+
+            // button all
             let btn = document.getElementsByTagName('button');
-            for (var j = 0; j < btn.length; j++) {
-                btn[j].style.color = '#000';
-                btn[j].style.border = '2px solid #000';
+            for (let j = 0; j < btn.length; j++) {
+                btn[j].style.color = '#fff';
+                btn[j].style.border = '2px solid #fff';
             }
-    
-            document.getElementsByClassName('logo1')[0].querySelector('img').src = 'img/logo2.png';
-    
-            document.body.style.backgroundColor = '#fff';
-            wrap.style.color = '#000';
-    
-            document.querySelector('html').style.setProperty("--ac", "#000");
-            document.querySelector('html').style.setProperty("--bc", "#fff");
-    
-            document.getElementsByClassName('sc7')[0].style.backgroundColor = '#fffadb';
-    
-            document.getElementById('slider').querySelector('video').src = 'img/video4.mp4';
-    
-            document.getElementsByClassName('m1')[0].querySelector('img').src = 'img/img1m.jpg';
-            document.getElementsByClassName('m1')[1].querySelector('img').src = 'img/img2m.jpg';
-            document.getElementsByClassName('m1')[2].querySelector('img').src = 'img/img3m.jpg';
-    
-            document.getElementsByClassName('m2')[0].querySelector('img').src = 'img/test31.jpg';
-            document.getElementsByClassName('m3')[0].querySelector('img').src = 'img/img5m.jpg';
-    
-            document.getElementsByClassName('sc3')[0].style.backgroundImage = "url('img/test27.jpg')";
-    
-            document.getElementsByClassName('sc5')[0].querySelector('img').src = 'img/test26.jpg';
-    
-            document.getElementsByClassName('m6')[0].querySelector('img').src = 'img/test28.jpg';
-            document.getElementsByClassName('b3')[0].querySelector('h3').innerText = 'The Breakfast Clup'
-            document.getElementsByClassName('b3')[0].querySelector('p').innerText = '11 Southwark St, London SE1 1RQ'
-    
-            document.getElementsByClassName('m6')[1].querySelector('img').src = 'img/test29.jpg';
-            document.getElementsByClassName('b3')[1].querySelector('h3').innerText = 'Ozone Coffee Roasters'
-            document.getElementsByClassName('b3')[1].querySelector('p').innerText = '11 Leonard St, London EC2A 4AQ'
-    
-            document.getElementsByClassName('m6')[2].querySelector('img').src = 'img/test30.jpg';
-            document.getElementsByClassName('b3')[2].querySelector('h3').innerText = 'Half Cup'
-            document.getElementsByClassName('b3')[2].querySelector('p').innerText = '100-102 Judd St, London WC1H 9NT'
-    
-    
-    
-            document.getElementsByClassName('m7')[0].querySelector('img').src = 'img/test24-1.jpg';
-            document.getElementsByClassName('m8')[0].querySelector('img').src = 'img/test25-1.jpg';
-    
-            document.getElementsByClassName('logo2')[0].querySelector('img').src = 'img/logo2.png';
+
+            // logo & basic
+            document.getElementsByClassName('logo1')[0].querySelector('img').src = 'img/logo.png';
+
+            document.body.style.backgroundColor = '#000';
+            wrap.style.color = '#fff';
+
+            document.querySelector('html').style.setProperty("--ac", "#fff");
+            document.querySelector('html').style.setProperty("--bc", "#000");
+
+            document.getElementsByClassName('sc7')[0].style.backgroundColor = '#500000';
+            
+            // slider
+            document.getElementById('slider').querySelector('video').src = 'img/video3.mp4';
+            
+            // sc1
+            document.getElementsByClassName('m1')[0].querySelector('img').src = 'img/img1.jpg';
+            document.getElementsByClassName('m1')[1].querySelector('img').src = 'img/img2.jpg';
+            document.getElementsByClassName('m1')[2].querySelector('img').src = 'img/img3.jpg';
+            
+            // sc2
+            document.getElementsByClassName('m2')[0].querySelector('img').src = 'img/test11.jpg';
+            document.getElementsByClassName('m3')[0].querySelector('img').src = 'img/img5.jpg';
+
+            // sc3
+            document.getElementsByClassName('sc3')[0].style.backgroundImage = "url('img/test22.jpg')";
+
+            // sc5
+            document.getElementsByClassName('sc5')[0].querySelector('img').src = 'img/test12.jpg';
+
+            //sc7
+            let img = ['img/test14.jpg', 'img/test15.jpg', 'img/test16.jpg'];
+            let r_name = ['The Coppa Club', 'The Wolseley', 'The IVY'];
+            let adress = ['3 Three Quays Walk, Lower Thames Street, London, EC3R 6AH', "160 Piccadilly, St. James's, London W1J 9EB", '1-5 West St, London WC2H 9NQ'];
+
+            for (let i = 0; i <img.length; i++) {
+                document.getElementsByClassName('m6')[i].querySelector('img').src = img[i];
+                document.getElementsByClassName('b3')[i].querySelector('h3').innerText = r_name[i];
+                document.getElementsByClassName('b3')[i].querySelector('p').innerText = adress[i];
+            }
+
+            //sc8
+            document.getElementsByClassName('m7')[0].querySelector('img').src = 'img/test24.jpg';
+            document.getElementsByClassName('m8')[0].querySelector('img').src = 'img/test25.jpg';
+
+            //footer
+            document.getElementsByClassName('logo2')[0].querySelector('img').src = 'img/logo.png';
         }
     }
 }
